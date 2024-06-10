b96af2705eb20066855b7704fae8b867a585c721
aliceh1 <149112426+ahobbs-716@users.noreply.github.com>
2024-06-10 11:12:59 -0400

    Updates to testing

--- FindClassmatesTest.java
+++ FindClassmatesTest.java
@@ -1,5 +1,7 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 
@@ -61,8 +63,9 @@ public class FindClassmatesTest {
 		}
 		
 	};
-	
 
+
+	//tests for basic functionality
 	@Test
 	public void testFindOneFriend() { 
 		
@@ -73,7 +76,6 @@ public class FindClassmatesTest {
 		assertTrue(response.contains("B"));
 
 	}
-
 	@Test
 	public void testFindNoFriends() { 
 		
@@ -83,14 +85,182 @@ public class FindClassmatesTest {
 		assertTrue(response.isEmpty());
 
 	}
-	
+
+
+	//test inputs to the method
+	@Test (expected = IllegalArgumentException.class)
+	public void testInputStudentIsNull() {
+
+		ff = new FriendFinder(defaultClassesDataSource, defaultStudentsDataSource);
+		ff.findClassmates(null);
+
+	}
+	@Test (expected = IllegalArgumentException.class)
+	public void testInputStudentHasNoName() {
+
+		ff = new FriendFinder(defaultClassesDataSource, defaultStudentsDataSource);
+		Set<String> response = ff.findClassmates(new Student(null, 101));
+
+	}
+
+
+	//test objects upon which the method depends
+	@Test (expected = IllegalStateException.class)
+	public void testNoStudentDataSourceObject() {
+
+		ff = new FriendFinder(defaultClassesDataSource, null);
+		Set<String> response = ff.findClassmates(new Student("A", 101));
+
+	}
+	@Test (expected = IllegalStateException.class)
+	public void testNoClassesDataSourceObject() {
+
+		ff = new FriendFinder(null, defaultStudentsDataSource);
+		assertEquals(Collections.EMPTY_SET, ff.findClassmates(new Student("test student", 1032)));
+
+	}
+
+
+	//test faulty objects are returned from invoked methods
 	@Test
-	public void testClassesDataSourceReturnsNullForInputStudent() { 
-		
+	public void testClassesDataSourceReturnsNullForInputStudent() {
+
 		ff = new FriendFinder(defaultClassesDataSource, defaultStudentsDataSource);
 		Set<String> response = ff.findClassmates(new Student("D", 104));
 		assertNotNull(response);
 		assertTrue(response.isEmpty());
 
 	}
+	@Test
+	public void testMyClassHasNullStudents() {
+
+		StudentsDataSource faultyStudentsReturn = new StudentsDataSource() {
+
+			@Override
+			public List<Student> getStudents(String className) {
+
+				Student a = new Student("A", 101);
+				Student b = new Student("B", 102);
+				Student c = new Student("C", 103);
+
+				if (className.equals("1")) {
+					return null;
+				}
+				else if (className.equals("2")) {
+					return List.of(a, b, c);
+				}
+				else if (className.equals("3")) {
+					return List.of(a, b);
+				}
+				else if (className.equals("4")) {
+					return List.of(c);
+				}
+				else return null;
+			}
+
+		};
+
+		ff = new FriendFinder(defaultClassesDataSource, faultyStudentsReturn);
+		Set<String> response = ff.findClassmates(new Student("A", 101));
+
+		//ignores faulty input
+		//therefore returns null (as only 1 class, which is faulty)
+		assertNotNull(response);
+		assertTrue(response.isEmpty());
+
+	}
+	@Test
+	public void testMyColleagueHasNullName() {
+
+		StudentsDataSource unnamedStudents = new StudentsDataSource() {
+			@Override
+			public List<Student> getStudents(String className) {
+
+				List<Student> students = new ArrayList<>();
+				students.add(new Student(null, 2042));
+
+				return students;
+			}
+		};
+		ff = new FriendFinder(defaultClassesDataSource, unnamedStudents);
+		Set<String> response = ff.findClassmates(new Student("A", 101));
+
+		//runs method ignoring faulty input
+		assertNotNull(response);
+		assertEquals(0, response.size());
+	}
+	@Test
+	public void testMyColleagueHasNullClasses() {
+
+		ClassesDataSource nullClasses = new ClassesDataSource() {
+
+			@Override
+			public List<String> getClasses(String studentName) {
+
+				if (studentName.equals("A")) {
+					return List.of("1", "2", "3");
+				}
+				else if (studentName.equals("B")) {
+					return null;
+				}
+				else if (studentName.equals("C")) {
+					return List.of("1", "2", "3");
+				}
+				else return null;
+
+			}
+
+		};
+		ff = new FriendFinder(nullClasses, defaultStudentsDataSource);
+		Set<String> response = ff.findClassmates(new Student("A", 101));
+
+		//runs method ignoring faulty input
+		assertNotNull(response);
+		assertEquals(1, response.size());
+		assertTrue(response.contains("C"));
+
+	}
+	@Test
+	public void testMyClassesContainNullListing() {
+
+		ClassesDataSource nullClasses = new ClassesDataSource() {
+			@Override
+			public List<String> getClasses(String studentName) {
+
+				List<String> list = new ArrayList<>();
+				list.add(null);
+				list.add(null);
+
+				return list;
+			}
+		};
+
+		ff = new FriendFinder(nullClasses, defaultStudentsDataSource);
+		Set<String> response = ff.findClassmates(new Student("A", 101));
+		assertNotNull(response);
+		assertTrue(response.isEmpty());
+
+	};
+	@Test
+	public void testStudentListContainsNulls() {
+
+		StudentsDataSource studentsWithNulls = new StudentsDataSource() {
+			@Override
+			public List<Student> getStudents(String className) {
+
+				List<Student> students = new ArrayList<>();
+				students.add(null);
+				students.add(null);
+
+				return students;
+			}
+		};
+		ff = new FriendFinder(defaultClassesDataSource, studentsWithNulls);
+		Set<String> response = ff.findClassmates(new Student("A", 101));
+
+		assertNotNull(response);
+		assertTrue(response.isEmpty());
+
+	}
+
 }
--- FriendFinder.java
+++ FriendFinder.java
@@ -5,63 +5,98 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class FriendFinder {
-	
+
 	protected ClassesDataSource classesDataSource;
 	protected StudentsDataSource studentsDataSource;
-	
+
 	public FriendFinder(ClassesDataSource cds, StudentsDataSource sds) {
 		classesDataSource = cds;
 		studentsDataSource = sds;
 	}
-	
+
 	/*
-	 * This method takes a String representing the name of a student 
-	 * and then returns a set containing the names of everyone else 
+	 * This method takes a String representing the name of a student
+	 * and then returns a set containing the names of everyone else
 	 * who is taking the same classes as that student.
 	 */
 	public Set<String> findClassmates(Student theStudent) {
-		
+
+		if (classesDataSource == null || studentsDataSource == null) {
+			throw new IllegalStateException("null dependency detected");
+		}
+
+		if(theStudent == null) {
+			throw new IllegalArgumentException("bad input: student does not exist");
+		}
+
 		String name = theStudent.getName();
-		
+
+		if (name == null) {
+			throw new IllegalArgumentException("bad input: selected student does not contain name information");
+		}
+
 		// find the classes that this student is taking
 		List<String> myClasses = classesDataSource.getClasses(name);
+
 		if (myClasses == null) {
 			return Collections.emptySet(); // return empty Set if the student isn't taking any classes
 		}
-		
+
 		// use the classes to find the names of the students
 		Set<String> classmates = new HashSet<String>();
-		
+
+		//for each class that I am taking...
 		for (String myClass : myClasses) {
+
+			if (myClass == null) {
+				break;
+			}
+
 			// list all the students in the class
 			List<Student> students = studentsDataSource.getStudents(myClass);
-			
+
+			if (students == null) {
+				break; // ignore this class if it does not contain a valid set of students
+			}
+
+			//for each student in one of my classes...
 			for (Student otherStudent : students) {
-				
+
+				//check that they are properly recorded
+				if (otherStudent == null || otherStudent.getName() == null) {
+					break;	//if not, ignore them
+				}
+
 				// find the other classes that they're taking
 				List<String> theirClasses = classesDataSource.getClasses(otherStudent.getName());
-							
-				// see if all of the classes that they're taking are the same as the ones this student is taking
-				boolean allSame = true;
-				for (String c : myClasses) {
-					if (theirClasses.contains(c) == false) {
-						allSame = false;
-						break;
+
+				//check that we know what else they are taking
+				if (theirClasses != null) {
+
+					// see if all of the classes that they're taking are the same as the ones this student is taking
+					boolean allSame = true;
+
+					//now check their class list against mine
+					for (String c : myClasses) {
+						if (theirClasses.contains(c) == false) {    //ignore them if they are not in my class at any stage (nb ok for them to take additional classes)
+							allSame = false;
+							break;
+						}
 					}
-				}
-				
-				// if they're taking all the same classes, then add to the set of classmates
-				if (allSame) {
-					if (otherStudent.getName().equals(name) == false) {
-						classmates.add(otherStudent.getName());
+
+					// if they're taking all the same classes, then add to the set of classmates
+					if (allSame) {
+						if (otherStudent.getName().equals(name) == false) {        //check that I am not looking at myself
+							classmates.add(otherStudent.getName());
+						}
 					}
 				}
 			}
 
 		}
-				
+
 		return classmates;
 	}
-	
+
 
 }
